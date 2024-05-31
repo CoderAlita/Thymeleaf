a8e9e95c42f096d6d0a67d1eea456af13b471ef3
CoderAlita <priyankavisheshwar1018@gmail.com>
2024-05-31 12:30:16 +0530

    Add Thymeleaf iteration

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -16,5 +19,17 @@ public class HomeController {
         model.addAttribute("message", "Welcome to our wonder society");
         model.addAttribute("items", Arrays.asList("1 BHK", "2 BHK", "3 BHK"));
         return "home";
+    }
+
+    @GetMapping("/iter")
+    public String iterations(Model model){
+
+        ArrayList<String> list =new ArrayList<>();
+        list.add("Mahesh");
+        list.add("Kavita");
+        list.add("Jugnu");
+        model.addAttribute("list",list);
+
+        return "iter";
     }
 }
